0eb833f180b0592d3da57b6dd11e139f20aae110
alex <geniofuturo@gmail.com>
2015-03-24 10:41:46 -0300

    Update setup infos.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='lz-string-python',
+    name='python-lz-string',
     version='1.0',
     py_modules=['lzstring'],
     url='https://github.com/alexsilva/lz-string-python',
     license='MIT',
     author='alex',
     author_email='alex@fabricadigital.com.br',
-    description='lz-string for python 2.7 - 3.0'
+    description='lz-string for python 2.7.+, 3.+'
 )
